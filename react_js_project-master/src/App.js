c5ecaa91306a8d889583701627b679bf5caddccb
AnnaPrints <65970510+AnnaPrints@users.noreply.github.com>
2023-01-26 13:52:29 +0200

    Add files via upload

---
+++ react_js_project-master/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from "./components/Hello"
+
+function App() {
+  return (
+    <div className="App">
+      <Hello name={'Name'} age={22}/>
+    </div>
+  );
+}
+
+export default App;
+
+let value = require('path')
---
+++ react_js_project-master/src/components/Hello.js
@@ -0,0 +1,11 @@
+const Hello = ({name, age}) => {
+    return (
+        <>
+            <p>LALAL</p>
+            <p className={'App'}>{name}</p>
+            <p>{age}</p>
+        </>
+    )
+}
+
+export default Hello;
